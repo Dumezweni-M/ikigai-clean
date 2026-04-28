1edbb60bfeed00fbf7be069bf410cb4d831ad916
Dee watson <you@example.com>
2026-04-28 21:51:30 +0200

    Add navigation route to dashboard

--- src/navigation/RootNavigator.jsx
+++ src/navigation/RootNavigator.jsx
@@ -6,6 +6,8 @@ import Initiate from '../screens/Initiate';
 import Reflect from '../screens/Update';
 import Create from '../screens/Create';
 import Catalysts from '../screens/Catalysts';
+import Dashboard from '../screens/Dashboard'
+
 
 
 const Stack = createNativeStackNavigator();
@@ -13,12 +15,13 @@ const Stack = createNativeStackNavigator();
 export default function RootNavigator() {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="Reflect" component={Reflect} />
-      <Stack.Screen name="Create" component={Create} />
-      <Stack.Screen name="Home" component={Home} />
-      <Stack.Screen name="Initiate" component={Initiate} />
-      <Stack.Screen name="Welcome" component={Welcome} />
+      <Stack.Screen name="Dashboard" component={Dashboard} />
       <Stack.Screen name="Catalysts" component={Catalysts} />
+      <Stack.Screen name="Welcome" component={Welcome} />
+      <Stack.Screen name="Home" component={Home} />
+      <Stack.Screen name="Create" component={Create} />
+      <Stack.Screen name="Reflect" component={Reflect} />
+      <Stack.Screen name="Initiate" component={Initiate} />
       <Stack.Screen name="Update" component={Reflect} />
       {/* {/* <Stack.Screen name="World" component={World} /> */}
     </Stack.Navigator>
