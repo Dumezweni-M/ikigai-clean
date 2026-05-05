64a4f1faa03fb898775f4c370c9a78ee0850e52a
Dee watson <you@example.com>
2026-05-05 17:16:28 +0200

    Standard operation setup

--- src/navigation/RootNavigator.jsx
+++ src/navigation/RootNavigator.jsx
@@ -15,14 +15,13 @@ const Stack = createNativeStackNavigator();
 export default function RootNavigator() {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="Welcome" component={Welcome} />
       <Stack.Screen name="Home" component={Home} />
       <Stack.Screen name="Activity" component={Activity} />
       <Stack.Screen name="Overview" component={Overview} />
       <Stack.Screen name="Create" component={Create} />
       <Stack.Screen name="Catalysts" component={Catalysts} />
-      <Stack.Screen name="Welcome" component={Welcome} />
       <Stack.Screen name="Initiate" component={Initiate} />
-      {/* {/* <Stack.Screen name="World" component={World} /> */}
     </Stack.Navigator>
   );
 }
